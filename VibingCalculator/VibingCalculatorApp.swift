03ab334e64cbc5acc8d7c9b96afb750c3a8cabff
Osman Kahraman <o_kahraman@outlook.com>
2026-06-11 12:05:47 -0400

    Initial Commit

---
+++ VibingCalculator/VibingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibingCalculatorApp.swift
+//  VibingCalculator
+//
+//  Created by Osman Kahraman on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct VibingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
